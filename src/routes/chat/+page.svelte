a8b53ad24fa520180a6612e35564da18e5d096d9
Robski87 <rbsthh@gmail.com>
2026-03-04 18:54:02 +0100

    Create +page.svelte

---
+++ src/routes/chat/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { browser } from '$app/environment';
+  import { goto } from '$app/navigation';
+
+  if (browser) {
+    goto('/chat/angest');
+  }
+</script>
+
+<svelte:head>
+  <title>Chat - MittPsyke</title>
+</svelte:head>
+
+<main style="padding:16px">
+  <p>Skickar dig till chatten...</p>
+  <p><a href="/chat/angest">Oppna chatten</a></p>
+</main>
